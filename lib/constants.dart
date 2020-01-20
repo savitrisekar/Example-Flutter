2f35220b37cfb0ed9d2c717517471aa0a18f011c
savitrisekar <savitrisekar10@gmail.com>
2020-01-20 16:22:40 +0700

    [sekar] update login

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const underlineTextField = Color(0xFF0075BF);
+  static const hintColor = Color(0xFFB3BBC8);
+}
--- lib/landing/login_page.dart
+++ lib/landing/login_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:basic_flutter/constants.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -7,20 +7,86 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = new GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      backgroundColor: Colors.white,
-      resizeToAvoidBottomPadding: true,
-      body: Stack(
-        fit: StackFit.expand,
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            _showForm(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _showForm() {
+    return new Container(
+        child: new Form(
+      key: _formKey,
+      child: new ListView(
+        shrinkWrap: true,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 5.0),
-            child: Image.asset("assets/images/img_logo_app.png"),
-          ),
+          showLogo(),
+          showUsernameInput(),
+          showPasswordInput(),
         ],
+      ),
+    ));
+  }
+
+  Widget showLogo() {
+    return Align(
+      alignment: Alignment.topRight,
+      child: Image.asset('assets/images/img_login_app.png'),
+    );
+  }
+
+  Widget showUsernameInput() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 100.0, left: 20.0, right: 20.0),
+      child: new TextFormField(
+        maxLines: 1,
+        keyboardType: TextInputType.text,
+        autofocus: false,
+        decoration: new InputDecoration(
+          hintText: 'Username',
+        ),
+        validator: (value) => value.isEmpty ? 'Username can\'t be empty' : null,
+//        onSaved: (value) => _username = value.trim(),
+      ),
+    );
+  }
+
+  Widget showPasswordInput() {
+    return Padding(
+      padding: const EdgeInsets.only(
+          top: 15.0, left: 16.0, right: 20.0, bottom: 20.0),
+      child: new TextFormField(
+        maxLines: 1,
+        obscureText: true,
+        autofocus: false,
+        decoration: const InputDecoration(
+          border: UnderlineInputBorder(),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: ColorPalette.underlineTextField,
+              width: 1.5,
+            ),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.black26,
+              width: 3.0,
+            ),
+          ),
+          hintText: 'Password',
+          hintStyle: TextStyle(color: ColorPalette.hintColor),
+        ),
+        validator: (value) => value.isEmpty ? 'Password can\'t be wmpty' : null,
       ),
     );
   }
